b539b56f15bbc3162fc6d2a7f9aa6eed89c6c1b6
EnemyBot3 <102303430+EnemyBot3@users.noreply.github.com>
2024-06-21 15:54:01 +0100

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy',
-      '^@/(.*)$': '<rootDir>/$1'
-    },
-    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-};
